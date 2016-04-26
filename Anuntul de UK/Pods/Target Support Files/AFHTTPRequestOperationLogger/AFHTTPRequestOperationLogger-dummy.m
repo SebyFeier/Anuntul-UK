27b1187f29f8010ee0d507b062a38c98fc936eee
Seby Feier <feier.sebastian@gmail.com>
2016-04-26 20:44:54 +0300

    no message

--- Anuntul de UK/Pods/Target Support Files/AFHTTPRequestOperationLogger/AFHTTPRequestOperationLogger-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AFHTTPRequestOperationLogger : NSObject
-@end
-@implementation PodsDummy_AFHTTPRequestOperationLogger
-@end
--- Anuntul de UK/Pods/Target Support Files/Pods-Anuntul de UK/Pods-Anuntul de UK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Anuntul_de_UK : NSObject
-@end
-@implementation PodsDummy_Pods_Anuntul_de_UK
-@end
